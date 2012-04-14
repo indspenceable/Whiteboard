5f5d4f0ef62ab40c2405582089ddbce437f243e8
Danny Spencer <daniel.patrick.spencer@gmail.com>
2012-04-13 20:09:34 -0700

    Updated to use mongodb serialization.

--- app.js
+++ app.js
@@ -1,45 +1,61 @@
-var express = require('express');
-var io = require('socket.io');
+var mongo = require('mongodb');
 
-var app = express.createServer()
-, io = io.listen(app);
+mongo.connect(process.env.MONGOLAB_URL, {}, function(error, db) {
+  var express = require('express');
+  var io = require('socket.io');
+  var app = express.createServer()
+  , io = io.listen(app);
 
-io.configure(function () { 
-  io.set("transports", ["xhr-polling"]); 
-  io.set("polling duration", 10); 
-});
-
-app.register('.jade', require('jade'));
-app.set('view options', {
-  layout: false
-})
-
-// Root to index page. Doesn't exist yet.
-app.get('/', function(req,res) {
-  res.render('index.jade');
-});
-// /(board) to that board.
-app.get('/:board', function(req,res) {
-  res.render('board.jade', {board: req.params.board});
-})
-
-app.listen(process.env.PORT || 5656);
-
-
-io.sockets.on('connection', function (socket) {
-  // on a join, add them to the right board and save that 
-  // property on their socket.
-  socket.on('join', function(data) {
-    console.log("Joining board ", data.board)
-    socket.set('board', data.board)
-    socket.join(data.board)
+  io.configure(function () { 
+    io.set("transports", ["xhr-polling"]); 
+    io.set("polling duration", 10); 
   });
 
-  // On a message, broadcast it to everyone in their board.
-  socket.on('draw', function(data) {
-    socket.get('board', function(err, board){
-      console.log("Broadcasting to ", board)
-      socket.broadcast.to(board).emit('draw', data)
-    })
+  app.register('.jade', require('jade'));
+  app.set('view options', {
+    layout: false
   })
+
+  // Root to index page. Doesn't exist yet.
+  app.get('/', function(req,res) {
+    res.render('index.jade');
+  });
+  // /(board) to that board.
+  app.get('/:board', function(req,res) {
+    res.render('board.jade', {board: req.params.board});
+  })
+
+  app.listen(process.env.PORT || 5656);
+
+  function insertPoints(board, points) {
+    points.forEach(function(point) {
+      if (db.points.find({x: point[0], y: point[0], board: board}).length == 0) {
+        // Add it
+        db.points.save({x: point[0], y: point[0], board: board})
+      }
+    })
+  }
+
+
+  io.sockets.on('connection', function (socket) {
+    // on a join, add them to the right board and save that 
+    // property on their socket.
+    socket.on('join', function(data) {
+      socket.set('board', data.board)
+      socket.join(data.board)
+      socket.emit('draw', {
+        points: db.points.find({board: data.board}).map(function(pt) {
+          [pt.x, pt.y]
+        })
+      })
+    });
+
+    // On a message, broadcast it to everyone in their board.
+    socket.on('draw', function(data) {
+      socket.get('board', function(err, board){
+        insertPoints(board,data.points)
+        socket.broadcast.to(board).emit('draw', data)
+      })
+    })
+  });
 });
